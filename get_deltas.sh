821011b88bf5e8403126e28c598d0122c7f70b15
dorian zhang <103979308+dorianzhang7@users.noreply.github.com>
2024-11-18 15:30:14 +0800

    Update get_deltas.sh

--- get_deltas.sh
+++ get_deltas.sh
@@ -1,3 +1,3 @@
 python scripts/get_deltas.py \
---path /workspace/code/fusion-diffusion3.0/models/fusion-diffusion3.0/2024-11-16T10-13-33_v2 \
+--path /workspace/code/fusion-diffusion3.0/models/fusion-diffusion3.0/the name of obtained model \
 --gpu_id 0
